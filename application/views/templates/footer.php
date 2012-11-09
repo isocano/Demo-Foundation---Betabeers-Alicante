45a843cbf71aa4fe88d94ca2f1f5831469083e88
Ignacio <ignacio@saldum.com>
2012-11-09 02:15:23 +0100

    jribbble 1.0.0

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,8 +1,30 @@
-  <!-- Included JS Files (Compressed) -->
-  <script src="<?php echo $base_url; ?>assets/frameworks/foundation/javascripts/jquery.js"></script>
-  <script src="<?php echo $base_url; ?>assets/frameworks/foundation/javascripts/foundation.min.js"></script>
-  
-  <!-- Initialize JS Plugins -->
-  <script src="<?php echo $base_url; ?>assets/frameworks/foundation/javascripts/app.js"></script>
+<?php $base_url = $this->config->item('base_url'); ?>
+
+	<script>
+		$(document).ready(function() {
+			$.jribbble.getShotsByList('popular', function (listDetails) {
+			    var html = [];
+			
+			    $.each(listDetails.shots, function (i, shot) {
+			        html.push('<li><h3>' + shot.title + '</h3>');
+			        html.push('<h4>by ' + shot.player.name + '</h4><a href="' + shot.url + '">');
+			        html.push('<img src="' + shot.image_teaser_url + '" ');
+			        html.push('alt="' + shot.title + '"></a></li>');
+			    });
+			
+			    $('#shotsByList').html(html.join(''));
+			}, {page: 1, per_page: 20});
+		});
+	</script>
+
+	<!-- Included jribbble dile -->
+	<script src="<?php echo $base_url; ?>assets/js/jribbble/jquery.jribbble-1.0.0.ugly.js"></script>
+
+	<!-- Included JS Files (Compressed) -->
+	<script src="<?php echo $base_url; ?>assets/frameworks/foundation/javascripts/jquery.js"></script>
+	<script src="<?php echo $base_url; ?>assets/frameworks/foundation/javascripts/foundation.min.js"></script>
+
+	<!-- Initialize JS Plugins -->
+	<script src="<?php echo $base_url; ?>assets/frameworks/foundation/javascripts/app.js"></script>
 </body>
 </html>
--- application/views/welcome_view.php
+++ application/views/welcome_view.php
@@ -1,3 +1,4 @@
 <div class="row">
 	<h1>Dribbble Popular</h1>
+	<div id="shotsByList"></div>
 </div>
