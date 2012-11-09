d11770e7392cbc4e21926510fc2be6d635d84ed4
Ignacio <ignacio@saldum.com>
2012-11-09 01:54:43 +0100

    Foundation 3.2

---
+++ application/views/templates/footer.php
@@ -0,0 +1,8 @@
+  <!-- Included JS Files (Compressed) -->
+  <script src="<?php echo $base_url; ?>assets/frameworks/foundation/javascripts/jquery.js"></script>
+  <script src="<?php echo $base_url; ?>assets/frameworks/foundation/javascripts/foundation.min.js"></script>
+  
+  <!-- Initialize JS Plugins -->
+  <script src="<?php echo $base_url; ?>assets/frameworks/foundation/javascripts/app.js"></script>
+</body>
+</html>
